b645679bd942f959f8eb4b3a4e77e983ed0751d3
Luiz Carlos <argentinaluiz@gmail.com>
2016-10-03 22:34:54 -0300

    Update UsersRepositoryFactory.php

--- module/CodeOrders/src/V1/Rest/Users/UsersRepositoryFactory.php
+++ module/CodeOrders/src/V1/Rest/Users/UsersRepositoryFactory.php
@@ -15,28 +15,10 @@ use Zend\Db\ResultSet\HydratingResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\ServiceManager\Exception\ServiceNotCreatedException;
 use Zend\ServiceManager\Exception\ServiceNotFoundException;
-use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class UsersRepositoryFactory implements FactoryInterface
+class UsersRepositoryFactory
 {
-
-    /**
-     * Create service
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return mixed
-     */
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        $dbadapter = $serviceLocator->get('dummy');
-        $userMapper = new UsersMapper();
-        $hydrator = new HydratingResultSet($userMapper, new UsersEntity());
-        $tablegateway = new TableGateway('oauth_users', $dbadapter, null, $hydrator);
-        $usersrepository = new UsersRepository($tablegateway);
-        return $usersrepository;
-    }
-
     /**
      * Create an object
      *
@@ -49,8 +31,13 @@ class UsersRepositoryFactory implements FactoryInterface
      *     creating a service.
      * @throws ContainerException if any other error occurs
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    public function __invoke(ContainerInterface $container)
     {
-        // TODO: Implement __invoke() method.
+        $dbadapter = $serviceLocator->get('dummy');
+        $userMapper = new UsersMapper();
+        $hydrator = new HydratingResultSet($userMapper, new UsersEntity());
+        $tablegateway = new TableGateway('oauth_users', $dbadapter, null, $hydrator);
+        $usersrepository = new UsersRepository($tablegateway);
+        return $usersrepository;
     }
 }
